4643dabf044e8685351f04694b55daa8d807bd2d
frizynn <101231690+frizynn@users.noreply.github.com>
2023-04-01 12:20:15 -0300

    Prueba

--- TP_003.py
+++ TP_003.py
@@ -1,26 +1,24 @@
-
-# Ejercicio 2 - Máximo rendimiento posible
-x0 = 100
+# Se definen las constantes dadas en el problema
+x0 = 237
+y0 = 4980 # Es importante destacar que el valor de y0 debe estar dentro del rango de 125 y 23142 para que el código funcione correctamente.
 alfa = 0.000082
 beta = 24487
 gama = 0.1
 dias = 90
 
-while True:
-    y0 = beta * 0.9 
-    x0+=1
-
-    for i in range(1,dias+1):   #
-        y_day2 = int(alfa * y0* (beta-y0) - gama *y0 - x0)
-        y0 += y_day2
-        if y0 <= 0:
-            x0-=1 #Se hace esta cuenta ya que el anterior x0 es el ultimo en el que el codigo da 0 en el dia 90
-            break 
-    if y0 <= 0:
-        break
-#El código utiliza un bucle while que se ejecuta hasta que la población de peces llega a ser igual o menor a cero, lo que indicaría la extinción de la población. Dentro del bucle, se utiliza un bucle for para simular la población durante los 90 días. En cada día, se calcula la cantidad de peces que mueren debido a la pesca ilegal, representada por la variable x0, y la variación de la población de peces, representada por la variable y_day2. Cuando el bucle for termina y la poblacion no se extinguió para el dia 90, se seuma 1 a la cantiad de pesca, pero con la misma población inical. La cantidad de peces del día actual se actualiza sumando y_day2 a la cantidad de peces del día anterior.
-
-print(f"La cantidad máxima de pesca diaria es: {x0}" ) 
-   
-
-
+# Se utiliza un ciclo while para buscar la población mínima viable
+while True: 
+    for i in range(dias+1): 
+        # Se calcula la población del día siguiente utilizando la ecuación dada
+        y_day2 = int(alfa * y0* (beta-y0) - gama *y0 - x0) 
+        # Si la población del día siguiente no es 0 o negativa, se reduce la población inicial en 1 y se calcula de nuevo
+        if y_day2:
+            y0-=1 
+        else:
+            break # Si la población calculada para el día siguiente es igual o menor a cero, el ciclo for actual terminará su ejecución y se detendrá el programa ya que no tiene sentido seguir evaluando una población que ya no es viable. Es decir, se ahorra 'tiempo'.
+    
+    if not y_day2:
+        break # Si se alcanza la población mínima viable, se rompe el ciclo while
+    
+# Se imprime el resultado
+print(f"La poblacion minima viable es: {y0}")
